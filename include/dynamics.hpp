12c6b7ee6c25d16db5721d79abcd7acb43664b18
QuiteFiber93 <quitefiber@QuiteFiberLT.>
2026-04-02 13:10:07 -0400

    dynamics jacobian structure

--- include/dynamics.hpp
+++ include/dynamics.hpp
@@ -21,7 +21,23 @@ Eigen::VectorXd dynamics(const double t, const Eigen::VectorXd& y, const double 
 }
 
 Eigen::MatrixXd dynamics_jacobian(double t, const Eigen::VectorXd& y, const double mu){
-    Eigen::MatrixXd jacobian = Eigen::MatrixXd::Zero(static_cast<int>(y.size()), static_cast<int>(y.size()));
+
+    (void)t;
+    int n = static_cast<int>(y.size());
+    Eigen::MatrixXd jacobian = Eigen::MatrixXd::Zero(n, n);
+
+    Eigen::Vector3d r = y.head(3);
+    double r_norm = r.norm();
+
+    // Jacobian has the structure
+    // [0       | I]
+    // [dr/dr   | 0]
+
+    jacobian.block(0, 3, 3, 3) = Eigen::Matrix3d::Identity();
+    jacobian.block(3, 0, 3, 3) = - mu / (r_norm * r_norm * r_norm) * Eigen::Matrix3d::Identity()
+        + 3 * mu / (r_norm * r_norm * r_norm * r_norm * r_norm) * (r * r.transpose());
+
+    return jacobian;
 }
 
 #endif
